fce93fd584874689d28a04996b77ce4e5c2d29f1
Selton Guedes <sfgs@ecomp.poli.br>
2021-04-12 08:56:23 -0300

    Adding a Readme file

--- public/script.js
+++
@@ -1,6 +0,0 @@
-/**
- * Babel: Converter (transpilar) cóigo do React para um código que o browser entenda
- * Webpack: Pra cada tipo de arquivo (.js, .css, .png) eu vou converter o código de uma maneira diferente
- *
- * Loaders: babel-loader, css-loader, image-loader
- */
