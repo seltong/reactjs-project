ef193ee7de56352253d899ae6a40de788ba278e0
Selton Guedes <sfgs@ecomp.poli.br>
2021-04-13 11:58:15 -0300

    Adding header properties

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Headers() {
+export default function Headers({ title, children }) {
+
   return (
     <header>
-      <h1>React JS</h1>
+      <h1>{title}</h1>
+
+      {children}
     </header>
   );
 }
