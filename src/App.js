474e287065086a01a6dfa08b0099118c2df742c9
Selton Guedes <sfgs@ecomp.poli.br>
2021-04-15 09:52:21 -0300

    Adding a method to create project

--- src/App.js
+++ src/App.js
@@ -15,8 +15,17 @@ function App() {
     });
   }, []);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Selton Guedes"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
